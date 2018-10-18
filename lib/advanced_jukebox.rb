324d4ad1e46ebc34672d8610465b774ed36f362c
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-18 23:06:09 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,10 +14,12 @@
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
-
-
 
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
